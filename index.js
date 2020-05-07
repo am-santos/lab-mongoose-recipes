93cfebface3a0bf5974380868c12f284c7e5eb30
am-santos <antopuppe@gmail.com>
2020-05-07 18:15:05 +0100

    Finished every iteration

--- index.js
+++ index.js
@@ -14,14 +14,40 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'My first recipe'
+    });
   })
-  .catch(error => {
+  .then((recipe) => {
+    console.log('Added recipe', recipe.title);
+
+    return Recipe.insertMany(data);
+  })
+  .then((data) => {
+    console.log('Added data.json recipes', data);
+
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+  })
+  .then((data) => {
+    console.log('Changed durantion of Rigatoni alla Genovese', data);
+
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then((data) => {
+    console.log('Carrot Cake is deleted', data);
+
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Disconnected from MongoDB');
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
